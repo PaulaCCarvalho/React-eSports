42952a2ed56d74bc0d333abf8c33862cddec55ec
Paula Cristina Carvalho Vilela <paulinhasgst@gmail.com>
2022-09-17 15:42:33 -0300

    Telas em desenvolvimento, conexão com o BD

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -10,9 +10,9 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        'gradient': 'linear-gradient(59.86deg, #9572FC 33.08%, #43E7AD 73.94%, #E1D55D 10.57%)',
-        'game-gradient': 'linear-gradient(188deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 67.88%)',
-        galaxy: "url('/background-galaxy.png')"
+        'gradient': 'linear-gradient(188deg, #9572FC 33.08%, #43E7AD 73.94%, #E1D55D 30.57%)',
+        'game-gradient': 'linear-gradient(188deg, rgba(0,0,0,0) 10%, rgba(0,0,0,0.9) 67.88%)',
+        galaxy: "url('/background-galaxy.png')",
       }
     },
   },
