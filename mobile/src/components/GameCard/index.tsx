719e3de36977c607a28f2ac5ca7d8df6c0578dda
Paula Cristina Carvalho Vilela <paulinhasgst@gmail.com>
2022-09-17 23:42:42 -0300

    Criando ligação com o BD e desenvolvendo o front

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { TouchableOpacity,
     TouchableOpacityProps, 
     ImageBackground, 
-    Text,
-    ImageSourcePropType 
+    Text
 } from 'react-native';
 
 import { LinearGradient } from 'expo-linear-gradient';
@@ -13,9 +12,11 @@ import { styles } from './styles';
 
 export interface GameCardProps{
     id: string;
-    name: string;
-    ads: string;
-    cover: ImageSourcePropType;
+    title: string;
+    _count: {
+        ads: number
+    };
+    bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps{
@@ -27,7 +28,7 @@ export function GameCard({data, ...rest}: Props) {
     <TouchableOpacity style={styles.container} {...rest}>
         <ImageBackground
             style={styles.cover}
-            source={data.cover}
+            source={{uri: data.bannerUrl}}
         >
 
             <LinearGradient
@@ -35,11 +36,11 @@ export function GameCard({data, ...rest}: Props) {
                 style={styles.footer}
             >
                 <Text style={styles.name} >
-                    {data.name}
+                    {data.title}
                 </Text>
 
                 <Text style={styles.ads}>
-                    {data.ads} anúncios
+                    {data._count.ads} anúncios
                 </Text>
             </LinearGradient>   
 
